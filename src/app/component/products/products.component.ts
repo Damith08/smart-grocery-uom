c186445126121c9f7d575ed58fa1df3d888dae31
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-07-11 08:12:53 +0530

    feat: angular bootstrap
    Using Bootstrap and CSS in Angular

--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-products',
-  template: '<div><h3>This is inline Template</h3></div>',
+  templateUrl: './products.component.html',
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent {}
