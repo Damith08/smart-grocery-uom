f3aeb3cb398e7774d8113493488c12d6cb6ba98c
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-07-11 06:19:37 +0530

    feat: angular component
    add a new component

---
+++ src/app/component/products/products.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  template: '<div><h3>This is inline Template</h3></div>',
+  styleUrls: ['./products.component.css'],
+})
+export class ProductsComponent {}
