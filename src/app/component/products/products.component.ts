7b6670299c913c6b5ebf5a8d4deef6c49578e1e8
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-07-14 09:23:34 +0530

    feat: angular directives
    Angular Directives

--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -6,10 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent {
-  public firstProductItemName = 'White Basmathi Rice';
-  public quantityOfRice = 75;
+  public products = [
+    {
+      productId: '001',
+      productName: 'White Basmathi Rice',
+      createdDate: 'Jan 29, 2023',
+      unitPrice: 'Rs. 400',
+      quantity: 100,
+      productDescription: 'White Basmathi Rice imported from Pakistan',
+    },
+    {
+      productId: '002',
+      productName: 'Flour',
+      createdDate: 'Jan 29, 2023',
+      unitPrice: 'Rs. 190',
+      quantity: 50,
+      productDescription: 'Super Fine whole grain general purpose flour',
+    },
+    {
+      productId: '003',
+      productName: 'Sugar',
+      createdDate: 'Jan 29, 2023',
+      unitPrice: 'Rs. 200',
+      quantity: 1200,
+      productDescription: 'White sugar manufactured by Palwatte Factory',
+    },
+    {
+      productId: '004',
+      productName: 'White Basmathi Rice',
+      createdDate: 'Jan 29, 2023',
+      unitPrice: 'Rs. 200',
+      quantity: 10,
+      productDescription: 'Imported mysoor dahl from India',
+    },
+  ];
 
-  public getQuantityOfRice() {
-    return 350;
-  }
+  // public isLowInventory = false;
+  // public dahlStorage = 10;
+
+  // public getDahlStorage() {
+  //   if (this.dahlStorage < 50) {
+  //     this.isLowInventory = true;
+  //   }
+  //   return this.dahlStorage;
+  // }
 }
